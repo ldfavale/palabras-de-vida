f748003ec5c79dd436a2024e21356de535731347
Leonardo Favale <ldfavale@gmail.com>
2024-08-26 15:04:35 -0300

    Improve Shop Page Section

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 
 function ShopItem({product}) {
   return (
-    <div className=" w-full ">
-        <div className=" ">
-            <img src={product.image} alt="product" className="w-full"/>
+    <div className=" max-w-md sm:hover:[box-shadow:3px_6px_6px_3px_rgba(0,0,0,0.15)] m-3  sm:w-1/3">
+        <div className="flex flex-col">
+            <div className=' flex sm:h-[231px] bg-lightgrey items-center justify-center'>
+                <img src={product.image} alt="product" className="w-full "/>
+            </div>
+            <div className=' -mt-6 flex items-end justify-end pr-4'>
+                <div className='rounded-full bg-primary border  border-white p-2'>
+                    <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5"/>
+                    </svg>
+                </div>
+            </div>
         </div>
-        <div className=" h-48 p-4">
+        <div className=" p-4">
             <p className="font-graphik font-medium text-black mb-2">{product.title}</p>
             <div className="flex flex-row  justify-between w-full mb-1 [&>span]:text-lightgrey [&>span]:font-thin  [&>span]:text-xs">
                 <span>{product.category}</span>
--- src/pages/ShoppingPage.jsx
+++ src/pages/ShoppingPage.jsx
@@ -17,10 +17,29 @@ const products = [{
   price: "UYU 2,200",
   image: "https://firebasestorage.googleapis.com/v0/b/kyte-7c484.appspot.com/o/2Cl4UdQWu4UlaOq6oRcosQCt7nI2%2FD888939D-F793-42A0-8113-3BD99A7D441F.jpg?alt=media"
 
-}]
+},
+{
+  title: 'Bingo “Salmo 34” Bilingüe',
+  category: "Juegos",
+  codigo: "COD: 9781623878030",
+  description: "Explora algunos capítulos de la Biblia mientras disfrutas de este juego con tu familia y amigos. Incluye: 8 tableros 72 tarjetas De 3 años en adelante De 2 a 8 jugadores ",
+  price: "UYU 297",
+  image: "https://firebasestorage.googleapis.com/v0/b/kyte-7c484.appspot.com/o/2Cl4UdQWu4UlaOq6oRcosQCt7nI2%2FACA1F1FE-8184-4368-8200-1055447DDB6B.jpg?alt=media"
+
+},
+{
+  title: 'Bingo “Salmo 34” Bilingüe',
+  category: "Juegos",
+  codigo: "COD: 9781623878030",
+  description: "Explora algunos capítulos de la Biblia mientras disfrutas de este juego con tu familia y amigos. Incluye: 8 tableros 72 tarjetas De 3 años en adelante De 2 a 8 jugadores ",
+  price: "UYU 297",
+  image: "https://firebasestorage.googleapis.com/v0/b/kyte-7c484.appspot.com/o/2Cl4UdQWu4UlaOq6oRcosQCt7nI2%2FACA1F1FE-8184-4368-8200-1055447DDB6B.jpg?alt=media"
+
+},
+]
 function ShoppingPage() {
   return (
-    <div className="pt-20">
+    <div className="p-5 pt-28 flex flex-col sm:flex-row items-center sm:items-start sm:justify-between min-h-screen " >
     { products.map(p => <ShopItem product={p}/>) }
       
     </div>
