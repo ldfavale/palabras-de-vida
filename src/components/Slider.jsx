74e86dc36fa12075da76e283b58526798ae54b27
Leonardo Favale <ldfavale@gmail.com>
2024-08-26 11:57:56 -0300

    Improve slider photos

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,7 +3,7 @@
 import '../App'
 import HeroSlider, { Overlay, Slide, Nav } from "hero-slider";
 import slide1 from '../assets/images/slide1.jpg'
-import slide2 from '../assets/images/slide2.png'
+import slide2 from '../assets/images/slide2.jpg'
 // import slide3 from '../assets/images/slide3.jpg'
 
 
