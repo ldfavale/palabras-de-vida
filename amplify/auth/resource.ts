ea1b878850de46754198a04c5c654cf2bc7ec538
Leonardo Favale <ldfavale@gmail.com>
2024-12-09 18:23:10 -0300

    Add Model Product

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,29 @@
-import { defineAuth } from "@aws-amplify/backend"
+import { a, defineAuth, defineData, type ClientSchema } from '@aws-amplify/backend';
+
+const schema = a.schema({
+  Product: a.model({
+      title: a.string(),
+      description: a.string(),
+      category: a.string(),
+      image: a.string(),
+      code: a.string(),
+      price: a.string(),
+      hola: a.string(),
+    })
+    .authorization(allow => [allow.publicApiKey()])
+});
+
+// Used for code completion / highlighting when making requests from frontend
+export type Schema = ClientSchema<typeof schema>;
+
+// defines the data resource to be deployed
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 }
+  }
+});
 
 /**
  * Define and configure your auth resource
