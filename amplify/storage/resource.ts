bbf82220ce712a8f1a77b259135310682c9c20ba
Leonardo Favale <ldfavale@gmail.com>
2025-01-17 14:04:20 -0300

    Add S3 bucket for product images

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'productImages' 
+  name: 'appMedia',
+  access: (allow) => ({
+    'product-images/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+  })
 });
