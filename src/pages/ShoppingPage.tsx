aa77e07bf6862f613fe667207b222088115f5fea
Leonardo Favale <ldfavale@gmail.com>
2025-05-03 02:08:03 -0300

    Fix Double containar issue

--- src/pages/ShoppingPage.tsx
+++ src/pages/ShoppingPage.tsx
@@ -1,9 +1,9 @@
 import useGetProducts from "../hooks/useGetProducts";
 import ShopItem from "../components/ShopItem";
 import Sidebar from "../components/ShopSidebar";
-import { ProductListSkeleton } from "../components/ProductListSkeleton";
 import toast from "react-hot-toast";
 import { useEffect } from "react";
+import { ProductListSkeleton } from "../components/ProductListSkeleton";
 
 
 function ShoppingPage() {
@@ -19,28 +19,28 @@ function ShoppingPage() {
 
   return (
     <div className="flex justify-center w-full  ">
-      <div className="sm:flex flex-1 sm:flex-row  justify-center w-full lg:max-w-6xl ">      
+      <div className="sm:flex flex-1 sm:flex-row  sm:justify-center w-full lg:max-w-6xl ">      
         <Sidebar/>
         {loading && ( <ProductListSkeleton count={12} />) }
 
         {!loading && !error && products.length === 0 && (
-             <div className="p-4 text-center text-gray-500">No se encontraron productos con esos criterios.</div>
+             <div className="p-4 text-center text-gray-500 pt-32">No se encontraron productos con esos criterios.</div>
         )}
-        
-         
+
         {!loading && !error && products.length > 0 && 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6"> 
-          {(
-          products.map((p) => (
-            <ShopItem key={p.id} product={p} />
-            ))
-          )}
+          <div className="container w-full  lg:px-4 py-8 pt-32 ">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6">  
+              {
+                products.map((p) => (
+                  <ShopItem key={p.id} product={p} />
+                ))
+              }
             </div>
-        }
-        {error && <div className="text-red-500 p-4">Error: {error.message}</div>}     
+          </div>
+        }        
       </div>
     </div>
-  );
-}
+    );
+  }
 
 export default ShoppingPage;
